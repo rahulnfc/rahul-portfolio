b1f11404d084abdc271ad81ca3fea1f23016bfbd
rahulnfc <rahulraghav093@gmail.com>
2021-01-28 20:07:30 +0530

    modified

--- javascript.js
+++ javascript.js
@@ -1,9 +1,50 @@
-$("#submit-form").submit((e)=>{
-    e.preventDefault()
+// $("#submit-form").submit((e)=>{
+//     e.preventDefault()
+//     $.ajax({
+//         url:"https://script.google.com/macros/s/AKfycbxDKckfuhDn_fYSNqBGtv3REiGvf1lY5I3BuZ3Lse-m-qcVQf4rF1Bd4w/exec",
+//         data:$("#submit-form").serialize(),
+//         method:"post",
+//         success:function (response){
+//             alert("Form submitted successfully")
+//             window.location.reload()
+//         },
+//         error:function (err){
+//             alert("Something Error")
+//         }
+//     })
+// })
+$("#submit-form").validate({
+    rules:{
+        Name:{
+            required: true,
+            minlength:3,
+            maxlength:10
+        },
+        Place:{
+            required: true,
+            minlength:3,
+            maxlength:10
+        },
+        Email:{
+            required: true,
+            email: true
+        },
+        Mobile:{
+            required: true,
+            number: true,
+            minlength:10,
+            maxlength:10
+        },
+        Message:{
+            required: true
+        }
+    },
+    submitHandler:(form, e)=>{
+    e.preventDefault();
     $.ajax({
         url:"https://script.google.com/macros/s/AKfycbxDKckfuhDn_fYSNqBGtv3REiGvf1lY5I3BuZ3Lse-m-qcVQf4rF1Bd4w/exec",
-        data:$("#submit-form").serialize(),
-        method:"post",
+        data:$("form").serialize(),
+        method:"POST",
         success:function (response){
             alert("Form submitted successfully")
             window.location.reload()
@@ -12,4 +53,5 @@ $("#submit-form").submit((e)=>{
             alert("Something Error")
         }
     })
-})
+}
+});
